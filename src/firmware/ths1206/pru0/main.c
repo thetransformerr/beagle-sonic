607810ca7375c057fd4994d98b9e6f1066dad4bf
Visaoni <visaoni@gmail.com>
2016-06-24 01:48:25 -0700

    Initial source commit

---
+++ src/firmware/ths1206/pru0/main.c
@@ -0,0 +1,20 @@
+/***************************************************************************************
+ * MAIN.C
+ *
+ * Description: Driver for PRU0 - THS1206
+ *
+ * (C) 2016 Visaoni
+ * Licensed under the MIT License.
+ **************************************************************************************/
+
+#include <stdint.h>
+#include "resource_table_empty.h"
+
+int main(void)
+{
+
+   __halt();
+
+   /* Should never return */
+   return 0;
+}
