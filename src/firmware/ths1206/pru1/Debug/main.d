35a1f4632c0a3eb22a986da90d803eba45d6783e
Visaoni <visaoni@gmail.com>
2016-06-24 13:08:12 -0700

    Added pin assignments and basic pin control.
    Will need future cleanup. pin_control.h is duplicated across PRU0 and 1.

---
+++ src/firmware/ths1206/pru1/Debug/main.d
@@ -0,0 +1,15 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-pru_2.1.2/include/stdint.h
+main.obj: ../resource_table_empty.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-pru_2.1.2/include/stddef.h
+main.obj: C:/ti/ccsv6/ccs_base/pru/include/rsc_types.h
+main.obj: C:/ti/ccsv6/ccs_base/pru/include/pru_types.h
+
+../main.c: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-pru_2.1.2/include/stdint.h: 
+../resource_table_empty.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-pru_2.1.2/include/stddef.h: 
+C:/ti/ccsv6/ccs_base/pru/include/rsc_types.h: 
+C:/ti/ccsv6/ccs_base/pru/include/pru_types.h: 
