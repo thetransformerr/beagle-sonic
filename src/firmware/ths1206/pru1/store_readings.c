224c5ea9548561af9ca5060d059b30a2c420a2e7
Visaoni <visaoni@gmail.com>
2016-06-24 22:11:04 -0700

    Implemented basic storage
    Stores results in 7KB of PRU1 RAM only. Could be worth looking at using
    shared PRU RAM in the future, but probably not. Still not enough to
    capture entire flight, and the current window isn't that small.

--- src/firmware/ths1206/pru1/store_readings.c
+++ src/firmware/ths1206/pru1/store_readings.c
@@ -10,14 +10,23 @@
 
 #include "store_readings.h"
 
-void SR_store( uint32_t reading )
+static uint32_t buffer[ SR_MAX_BUFFER_SIZE ];
+static uint32_t head = 0;
+
+uint32_t SR_store( uint32_t reading )
 {
-   // TODO: fill in
-   // Probably have to store to RAM, needs to be relatively quick
+   // TODO: Added in shared RAM too? Currently using just 7KB of PRU1 RAM
    // Limited size is a problem. Even with all of PRU1 RAM and Shared PRU RAM only ~5k readings
    // Max coverage of ~0.8 ms, rough estimate of average tof is ~1.5ms
+   // Current coverage is just under 0.3ms with 1750 readings
    // Will need to delay recording
-   return;
+
+   if( head < SR_MAX_BUFFER_SIZE )
+   {
+      buffer[head++] = reading;
+      return 0;
+   }
+   return 1;   // Buffer full
 }
 
 void SR_transfer_readings()
