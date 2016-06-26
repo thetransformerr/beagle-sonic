ce2e08d7d40e8280901009ca268d1c19170bc804
Visaoni <visaoni@gmail.com>
2016-06-26 11:54:03 +0000

    Fixed up permissions and copy

--- src/firmware/ths1206/pru0/deploy.sh
+++ src/firmware/ths1206/pru0/deploy.sh
@@ -2,6 +2,6 @@
 
 make clean
 make
-cp gen/*.out /lib/firmware/am335x-pru$PRU_CORE-fw
+cp gen/*.out /lib/firmware/am335x-pru0-fw
 echo "4a334000.pru0" > /sys/bus/platform/drivers/pru-rproc/unbind 2> /dev/null
 echo "4a334000.pru0" > /sys/bus/platform/drivers/pru-rproc/bind
--- src/firmware/ths1206/pru1/deploy.sh
+++ src/firmware/ths1206/pru1/deploy.sh
@@ -2,6 +2,6 @@
 
 make clean
 make
-cp gen/*.out /lib/firmware/am335x-pru$PRU_CORE-fw
+cp gen/*.out /lib/firmware/am335x-pru1-fw
 echo "4a338000.pru1" > /sys/bus/platform/drivers/pru-rproc/unbind 2> /dev/null
 echo "4a338000.pru1" > /sys/bus/platform/drivers/pru-rproc/bind
