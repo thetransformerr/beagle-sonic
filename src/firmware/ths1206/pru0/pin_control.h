6da7f2b7a411db5166270bec60481583fd560673
Visaoni <visaoni@gmail.com>
2016-07-05 23:30:41 -0700

    Now uses macros instead of functions
    Mostly this is due to a strange issue where the set_pin function wouldn't
    work, but clear_pin would. Inlining didn't help, but having the contents
    of the set_pin function copied in source works.

--- src/firmware/ths1206/pru0/pin_control.h
+++ src/firmware/ths1206/pru0/pin_control.h
@@ -15,15 +15,13 @@
 #include <stdint.h>
 
 
-void set_pin( uint32_t bit );
+volatile register uint32_t __R30;
+volatile register uint32_t __R31;
 
-void clear_pin( uint32_t bit );
-
-// TODO: Convert to 0/1?
-uint32_t read_pin( uint32_t bit );
-
-void write_reg( uint32_t value );
-
-uint32_t read_reg();
+#define set_pin( bit ) ( __R30 |= (uint32_t)1 << bit )
+#define clear_pin( bit ) ( __R30 &= (uint32_t)~(1 << bit ))
+#define read_pin( bit ) ( __R31 & (uint32_t)(1 << bit) )
+#define write_reg( value ) ( __R30 = value )
+#define read_reg() ( __R31 )
 
 #endif /* PIN_CONTROL_H_ */
