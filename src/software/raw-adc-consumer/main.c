5be0811c4a53e7da467b4bb7c6a6e7cfa21bec09
Visaoni <visaoni@gmail.com>
2016-07-05 20:53:18 -0700

    Initial commit for ADC consumer

---
+++ src/software/raw-adc-consumer/main.c
@@ -0,0 +1,12 @@
+/***************************************************************************************
+ * MAIN.C
+ *
+ * Description: Converts raw ADC reads given via character device to time of flight
+ *
+ * (C) 2016 Visaoni
+ * Licensed under the MIT License.
+ **************************************************************************************/
+int main(void)
+{
+	return 0;
+}
