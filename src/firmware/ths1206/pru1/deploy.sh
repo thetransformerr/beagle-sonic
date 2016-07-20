6623a6bee9f9f8560134c7981cd2e458fdabaac3
Visaoni <visaoni@gmail.com>
2016-07-20 00:12:07 +0000

    Stops PRU before build.
    When the PRU is sending a lot of messages, the system is significant taxed.
    This allows the build process to run much faster when the firmware is in the message loop.

--- src/firmware/ths1206/pru1/deploy.sh
+++ src/firmware/ths1206/pru1/deploy.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
+echo "4a338000.pru1" > /sys/bus/platform/drivers/pru-rproc/unbind 2> /dev/null
 make clean
 make
 cp gen/*.out /lib/firmware/am335x-pru1-fw
-echo "4a338000.pru1" > /sys/bus/platform/drivers/pru-rproc/unbind 2> /dev/null
 echo "4a338000.pru1" > /sys/bus/platform/drivers/pru-rproc/bind
