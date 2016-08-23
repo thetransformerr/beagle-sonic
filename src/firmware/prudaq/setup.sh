4533d66403afcd4baa1accade3dff9e741c7919b
Visaoni <visaoni@gmail.com>
2016-08-23 04:08:31 +0000

    Actually sets pinmux
    Device tree overlay isn't setting the pinmux. Instead use universal overlays
    and config-pin.

--- src/firmware/prudaq/setup.sh
+++ src/firmware/prudaq/setup.sh
@@ -30,23 +30,67 @@ fi
 # BeagleLogic's prudaq support instead.
 modprobe uio_pruss extram_pool_sz=2097152
 
-DEVICE_OVERLAY=/lib/firmware/prudaq-00A0.dtbo
-if [[ ! -f $DEVICE_OVERLAY ]] ; then
-  echo -n "$DEVICE_OVERLAY not found, so we can't enable it."
-  echo -n " (Did you run 'make install' to generate the .dtbo"
-  echo -n " file from the .dts and copy it to /lib/firmware?)"
-  echo
-  echo
-  exit 2
-fi
 
-if echo prudaq > /sys/devices/platform/bone_capemgr/slots ; then
-  echo 'Device overlay successfully loaded.'
-else
-  echo
-  echo -n "Couldn't enable the prudaq-00A0.dtbo device overlay."
-  echo -n " (If the error was 'File exists', then the overlay was already"
-  echo -n " enabled.)"
-  echo
-  exit 3
-fi
+# Overlays are the proper way, but currently there are some major issues with it
+# Namely it doesn't actually configure the pins
+
+# DEVICE_OVERLAY=/lib/firmware/prudaq-00A0.dtbo
+# if [[ ! -f $DEVICE_OVERLAY ]] ; then
+#   echo -n "$DEVICE_OVERLAY not found, so we can't enable it."
+#   echo -n " (Did you run 'make install' to generate the .dtbo"
+#   echo -n " file from the .dts and copy it to /lib/firmware?)"
+#   echo
+#   echo
+#   exit 2
+# fi
+
+# if echo prudaq > /sys/devices/platform/bone_capemgr/slots ; then
+#   echo 'Device overlay successfully loaded.'
+# else
+#   echo
+#   echo -n "Couldn't enable the prudaq-00A0.dtbo device overlay."
+#   echo -n " (If the error was 'File exists', then the overlay was already"
+#   echo -n " enabled.)"
+#   echo
+#   exit 3
+# fi
+
+# Instead do it with universal overlays and config-pin
+# Grabs the needed pins under a universal overlay so config-pin can touch them
+# Overlays must precede pins
+
+configlist=(
+"overlay univ-all"
+
+"P8.27 pruin"
+"P8.28 pruin"
+"P8.29 pruin"
+"P8.30 pruin"
+"P8.39 pruin"
+"P8.40 pruin"
+"P8.41 pruin"
+"P8.42 pruin"
+"P8.43 pruin"
+"P8.44 pruin"
+"P8.45 pruin"
+"P8.46 pruin"
+"P9.26 pruin"
+
+"P9.25 pruout"
+"P9.27 pruout"
+"P9.28 pruout"
+"P9.29 pruout"
+"P9.30 pruout"
+"P9.31 pruout"
+"P9.92 pruout"    # See note
+)
+
+# Note: P9.42 is referred to as P9.92 above.
+# The physical pin is shared by two processor pins. Add 50 to specify P9.42.1
+
+for args in "${configlist[@]}"
+do
+  config-pin $args
+done
+
+exit 0
