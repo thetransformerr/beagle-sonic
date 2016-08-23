1b4a71130cc2b9e12c496aef22e89657202b0add
Visaoni <visaoni@gmail.com>
2016-08-23 11:09:09 +0000

    Update for prudaq

--- Makefile
+++ Makefile
@@ -1,11 +1,7 @@
 .PHONY: all clean
 
 all:
-	cd src/firmware/ths1206/pru0 && $(MAKE)
-	cd src/firmware/ths1206/pru1 && $(MAKE)
-	cd src/software/adc-controller && $(MAKE)
+	cd src/firmware/prudaq && $(MAKE)
 
 clean:
-	cd src/firmware/ths1206/pru0 && $(MAKE) clean
-	cd src/firmware/ths1206/pru1 && $(MAKE) clean
-	cd src/software/adc-controller && $(MAKE) clean
+	cd src/firmware/prudaq && $(MAKE) clean
--- run.sh
+++ run.sh
@@ -1,29 +1,17 @@
 #!/bin/bash
 
-# TODO: Should probably stop the PRUs too
 trap 'kill $(jobs -pr)' SIGINT SIGTERM EXIT
 
 make
 
-# Shutdown both prus if running
-echo "4a334000.pru0" > /sys/bus/platform/drivers/pru-rproc/unbind 2> /dev/null
-echo "4a338000.pru1" > /sys/bus/platform/drivers/pru-rproc/unbind 2> /dev/null
+# Ensure uio_pruss module is up and pins configured
+./src/firmware/prudaq/setup.sh
 
-# Load the firmwares
-cp src/firmware/ths1206/pru0/gen/*.out /lib/firmware/am335x-pru0-fw &&
-cp src/firmware/ths1206/pru1/gen/*.out /lib/firmware/am335x-pru1-fw ||
-   exit 1
+# Start up the prudaq capture
+# TODO: At least take channels as input
+# TODO: Get somebody else to make this work
+./src/firmware/prudaq/prudaq_capture -i 0 -q 4 -o >(./src/firmware/prudaq/tof_analysis) pru0.bin pru1.bin &
 
-# Start up both prus
-echo "4a334000.pru0" > /sys/bus/platform/drivers/pru-rproc/bind
-echo "4a338000.pru1" > /sys/bus/platform/drivers/pru-rproc/bind
-
-# Wait for rpmsg driver to set up
-# TODO: maybe add a check that /dev/rpmsg_pru31 exists?
-sleep 0.5
-
-# Start up userspace adc driver
-./src/software/adc-controller/adc-controller.out >/dev/null &
 
 # Start up sensor fusion script
 ./src/software/sensor-fusion/fusion.py
