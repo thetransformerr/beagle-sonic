bb1e5c9604c7a3362387d90419eedb3236a44c41
Naveen Saini <thetransformerr@gmail.com>
2017-08-16 22:13:55 +0530

    Updated the multi axis part

--- multi_axis_analysis/process.py
+++ multi_axis_analysis/process.py
@@ -13,44 +13,54 @@ import time
 import numpy as np
 
 # TODO: Take port as input
-zmq_tof_host = "tcp://localhost:5555"
+zmq_sample_x_host = "tcp://localhost:5555"
+zmq_sample_y_host = "tcp://localhost:5556"
+zmq_temp_LM35_host ="tcp://localhost:5557"
+
+zmq_context_x = zmq.Context()
+zmq_sample_subscriber_x = zmq_context_x.socket( zmq.SUB )
+zmq_sample_subscriber_x.setsockopt( zmq.CONFLATE, 1 )           # Keeps only latest message
+zmq_sample_subscriber_x.connect( zmq_sample_x_host )
+zmq_sample_subscriber_x.setsockopt( zmq.SUBSCRIBE, "" )  # Listen to all messages on socket
 
 
-zmq_context = zmq.Context()
-zmq_sample_subscriber = zmq_context.socket( zmq.SUB )
-zmq_sample_subscriber.setsockopt( zmq.CONFLATE, 1 )           # Keeps only latest message
-zmq_sample_subscriber.connect( zmq_tof_host )
-zmq_sample_subscriber.setsockopt( zmq.SUBSCRIBE, "" )  # Listen to all messages on socket
+zmq_context_y = zmq.Context()
+zmq_sample_subscriber_y = zmq_context_y.socket( zmq.SUB )
+zmq_sample_subscriber_y.setsockopt( zmq.CONFLATE, 1 )           # Keeps only latest message
+zmq_sample_subscriber_y.connect( zmq_sample_y_host )
+zmq_sample_subscriber_y.setsockopt( zmq.SUBSCRIBE, "" )  # Listen to all messages on socket
 
-def get_samples(): #collect abt 1000 samples for each channel for rate at 200k samples and 
+
+zmq_context_temp = zmq.Context()
+zmq_sample_subscriber_temp = zmq_context_temp.socket( zmq.SUB )
+zmq_sample_subscriber_temp.setsockopt( zmq.CONFLATE, 1 )           # Keeps only latest message
+zmq_sample_subscriber_temp.connect( zmq_temp_LM35_host )
+zmq_sample_subscriber_temp.setsockopt( zmq.SUBSCRIBE, "" )  # Listen to all messages on socket
+
+def get_samples(zmq_sample_subscriber_XorY): #collect abt 2400 samples for each channel for rate at 200k samples and 
                    #axis distance of 15 cm
    samples_count=0
-   samples_x_tx_sensor=[]
-   samples_x_rx_sensor=[]
-   samples_y_tx_sensor=[]
-   samples_y_rx_sensor=[]
+   samples_tx_sensor=[]
+   samples_rx_sensor=[]
    temp=0
-   while(samples_count <10000):
-      temp=zmq_sample_subscriber.recv()
+   while(samples_count <5000):
+      temp=zmq_sample_subscriber_XorY
       temp=temp[1:]
       temp=hx.dump(temp1,size=4,sep=' ')
       temp=temp.split()
       print len(temp)
       for i in temp:
-         s=samples_count%4
+         s=samples_count%2
          if(s==0):
-            samples_x_rx_sensor.append(int(i,16))
+            samples_tx_sensor.append(int(i,16))
          elif(s==1):
-            samples_x_rx_sensor.append(int(i,16))
-         elif(s==2):
-            samples_y_tx_sensor.append(int(i,16))
-         elif(s==3):
-            samples_y_rx_sensor.append(int(i,16))
+            samples_rx_sensor.append(int(i,16))
          if samples_count<=10000:
             samples_count=samples_count+1
          else:
             break
-   return samples_y_rx_sensor,samples_y_tx_sensor,samples_x_rx_sensor,samples_x_tx_sensor
+   return samples_tx_sensor,samples_rx_sensor
+
 
 
 
@@ -62,8 +72,20 @@ def get_tof(sam_TX,sam_RX):
    tof=np.argmax(correlate_np)-(len(normalised_tx)/2)
    return tof
 
-def temp(sam_temp_sensor):
-   return temp # to implement working for sampling analog values from sensor like lm-35
+def get_temp(zmq_sample_subscriber_temp):
+   temp=zmq_sample_subscriber_temp
+   temp=temp[1:]
+   temp=hx.dump(temp1,size=4,sep=' ')
+   temp=temp.split()
+   counter=0
+   temperature=[]
+   for i in temp:
+      if (counter < 100) and (counter%2==0):
+         temperature.append(int(i,16))
+      else:
+         break
+   temp_mean=np.mean(temperature)
+   return temp_mean # to implement working for sampling analog values from sensor like lm-35
 
 
 # TODO: Take as input, or better yet get from configuration process
@@ -87,10 +109,11 @@ def find_windspeed( temp,tof ):
 
 def run():
    while True:
-      temp = get_temp
-      samples_y_rx_sensor,samples_y_tx_sensor,samples_x_rx_sensor,samples_x_tx_sensor=get_samples()
-      tof_x = get_tof(samples_x_tx_sensor,samples_x_rx_sensor)
-      tof_y=get_tof(samples_y_tx_sensor,samples_y_rx_sensor)
+      temp = get_temp(zmq_sample_subscriber_temp.recv())
+      samples_y_tx_sensor,samples_y_rx_sensor =get_samples(zmq_sample_subscriber_y.recv())
+      tof_y = get_tof(samples_y_tx_sensor,samples_y_rx_sensor)
+      samples_x_tx_sensor,samples_x_rx_sensor =get_samples(zmq_sample_subscriber_x.recv())
+      tof_x=get_tof(samples_x_tx_sensor,samples_x_rx_sensor)
       windspeed_x,direction_x=find_windspeed(temp,tof_x)
       windspeed_y,direction_y=find_windspeed(temp,tof_y)
       print( "Temperature: " + str(temp) )
